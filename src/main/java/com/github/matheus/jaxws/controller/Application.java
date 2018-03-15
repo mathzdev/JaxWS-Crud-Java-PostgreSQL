fe7eddc176a0a714decad1972ead6946d103e2a6
Matheus Lúcio <eu@ma-the.us>
2018-03-15 14:01:24 -0300

    Dinamizando endpoint de noticia

--- src/main/java/com/github/matheus/jaxws/controller/Application.java
+++ src/main/java/com/github/matheus/jaxws/controller/Application.java
@@ -12,7 +12,7 @@ public class Application {
         /*
          * Endpoint local
          */
-        ligarEndpointNoticia("http","jaxws-crud-java-postgresql.herokuapp.com", "8080");
+        ligarEndpointNoticia("http","localhost", "8080");
 	}
 
     /**
